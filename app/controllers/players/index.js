5fda3caf05b2d1ae9afe812a69eec071fa0cc767
evge_nik <evge_nik@tut.by>
2020-09-29 13:20:34 +0300

    Application: add pagination

--- app/controllers/players/index.js
+++ app/controllers/players/index.js
@@ -3,11 +3,23 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
+    get pageArray() {
+        let array = []
+
+        for (let index = 1; index <= Math.ceil(this.model.meta.count / this.limit); index++) {
+            array.push(index);
+        }
+
+        return array;
+    }
+
     queryParams = [
         'filterField',
         'filterValue',
         'sortField',
-        'sortDirection'
+        'sortDirection',
+        'page',
+        'limit'
     ];
 
     @tracked filterField;
@@ -15,12 +27,26 @@ export default class IndexController extends Controller {
     @tracked filter = "";
     @tracked sortField = 'id';
     @tracked sortDirection = 'ASC';
+    @tracked page = 1;
+    @tracked limit = 5;
+
+    arrayLimit = [5, 10, 20, 50];
 
     @action
     onHeaderClick(sortField) {
         this.sortField = sortField;
 
         this.sortDirection = (this.sortDirection == "ASC") ? "DESC" : "ASC";
+    }
+
+    @action
+    onPageClick(page) {
+        this.page = page;
+    }
+
+    @action
+    updateLimit(event) {
+        this.limit = event.target.value;
     }
 
     @action
