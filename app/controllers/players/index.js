03dd0a3261b8538bcb882ba22d95eca719cc6d68
evge_nik <evge_nik@tut.by>
2020-09-29 15:23:27 +0300

    Controllers: add const for sortDirection

--- app/controllers/players/index.js
+++ app/controllers/players/index.js
@@ -3,18 +3,23 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
+    ascendingDirection = 'ASC'
+    descendingDirection = 'DESC'
+
     queryParams = [
         'sortField',
         'sortDirection'
     ];
 
     @tracked sortField = 'id';
-    @tracked sortDirection = 'ASC';
+    @tracked sortDirection = this.ascendingDirection;
 
     @action
     onHeaderClick(sortField) {
         this.sortField = sortField;
 
-        this.sortDirection = (this.sortDirection == "ASC") ? "DESC" : "ASC";
+        this.sortDirection = (this.sortDirection == this.ascendingDirection)
+            ? this.descendingDirection
+            : this.ascendingDirection;
     }
 }
