e5ca1f1ed9ba7767853d9adccac5a217c8327645
evge_nik <evge_nik@tut.by>
2020-09-29 16:19:50 +0300

    Controllers: fix sorting const/conditions

--- app/controllers/players/index.js
+++ app/controllers/players/index.js
@@ -2,24 +2,24 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-export default class IndexController extends Controller {
-    ascendingDirection = 'ASC'
-    descendingDirection = 'DESC'
+const ascendingDirection = 'ASC';
+const descendingDirection = 'DESC';
 
+export default class IndexController extends Controller {
     queryParams = [
         'sortField',
         'sortDirection'
     ];
 
     @tracked sortField = 'id';
-    @tracked sortDirection = this.ascendingDirection;
+    @tracked sortDirection = ascendingDirection;
 
     @action
     onHeaderClick(sortField) {
         this.sortField = sortField;
 
-        this.sortDirection = (this.sortDirection == this.ascendingDirection)
-            ? this.descendingDirection
-            : this.ascendingDirection;
+        this.sortDirection = (this.sortDirection === ascendingDirection)
+            ? descendingDirection
+            : ascendingDirection;
     }
 }
