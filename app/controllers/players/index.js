1883d83a57a060d251369026f2c35b041143678d
evge_nik <evge_nik@tut.by>
2020-10-05 20:28:19 +0300

    Controllers: add tableData, refactor onFilterClick method

--- app/controllers/players/index.js
+++ app/controllers/players/index.js
@@ -4,6 +4,8 @@ import { tracked } from '@glimmer/tracking';
 
 const ASCENDING_DIRECTION = 'ASC';
 const DESCENDING_DIRECTION = 'DESC';
+const DOWN_ARROW = '&darr;';
+const UP_ARROW = '&uarr;';
 
 export default class IndexController extends Controller {
     get pageValues() {
@@ -21,9 +23,25 @@ export default class IndexController extends Controller {
 
     @tracked sortField = 'id';
     @tracked sortDirection = ASCENDING_DIRECTION;
+    @tracked sortDirectionArrow = DOWN_ARROW;
 
     @tracked page = 1;
     @tracked limit = 5;
+
+    tableData = [
+        {
+            description: 'ID',
+            fieldName: 'id'
+        },
+        {
+            description: 'First name',
+            fieldName: 'firstName'
+        },
+        {
+            description: 'Last name',
+            fieldName: 'lastName'
+        }
+    ]
 
     filterFieldValues = [
         'firstName',
@@ -33,12 +51,8 @@ export default class IndexController extends Controller {
     limitValues = [5, 10, 20, 50];
 
     @action
-    selectFilterField(filterField) {
-        this.filterField = filterField;
-    }
-
-    @action
     onFilterClick() {
+        this.filterField = this.field;
         this.filterValue = this.value;
 
         this.page = 1;
@@ -51,6 +65,10 @@ export default class IndexController extends Controller {
         this.sortDirection = (this.sortDirection === ASCENDING_DIRECTION)
             ? DESCENDING_DIRECTION
             : ASCENDING_DIRECTION;
+
+        this.sortDirectionArrow = (this.sortDirection === ASCENDING_DIRECTION)
+            ? DOWN_ARROW
+            : UP_ARROW;
 
         this.page = 1;
     }
@@ -71,7 +89,7 @@ export default class IndexController extends Controller {
 
     @action
     onPageIncrementClick() {
-        if (this.page == this.pageValues.length) {
+        if (this.page === this.pageValues.length) {
             return;
         }
 
