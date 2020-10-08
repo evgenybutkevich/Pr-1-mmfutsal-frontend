db58e53cdb73162b7e7f8768b3ed459645cb8e71
evge_nik <evge_nik@tut.by>
2020-10-08 17:23:24 +0300

    Application: add onCreateButtonClick method

--- app/controllers/players/index.js
+++ app/controllers/players/index.js
@@ -88,6 +88,11 @@ export default class IndexController extends Controller {
     }
 
     @action
+    onCreateButtonClick() {
+        this.transitionToRoute('players.create');
+    }
+
+    @action
     onViewButtonClick(id, event) {
         event.preventDefault();
         this.transitionToRoute('players.view', id);
