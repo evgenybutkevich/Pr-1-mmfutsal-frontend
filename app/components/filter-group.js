25ba1d293d0048bcf1acf8339116aa22fd86cf08
evge_nik <evge_nik@tut.by>
2020-10-26 10:45:32 +0300

    Components: fix filter-group code style

--- app/components/filter-group.js
+++ app/components/filter-group.js
@@ -2,6 +2,6 @@ import Component from '@glimmer/component';
 
 export default class FilterGroupComponent extends Component {
     get selectedObject() {
-        return this.args.selectOptions.find(object => object.value === this.args.filterFieldContent);
+        return this.args.selectOptions.find(({ value }) => { return value === this.args.filterFieldContent; });
     }
 }
