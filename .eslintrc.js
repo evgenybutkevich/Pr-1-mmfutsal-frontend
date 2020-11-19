05a2a1bbc888a8f26205604ea090ae82496cad22
Evgeny <evge_nik@tut.by>
2020-11-19 16:28:04 +0300

    Application: configure linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,31 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true,
+        es2021: true
     },
-    extends: [
-        'airbnb-base',
-    ],
+    extends: ['airbnb-base'],
     parserOptions: {
         ecmaVersion: 12,
-        sourceType: 'module',
+        sourceType: 'module'
     },
     rules: {
-        linebreak-style: 0,
-        global-require: 0,
-        eslint linebreak-style: [0, "error", "windows"],
-        
-        indent: ['error', 4],
-    },
+        'array-bracket-newline': ['error', 'never'],
+        'array-bracket-spacing': ['error', 'never'],
+        'arrow-body-style': ['error', 'always'],
+        'arrow-parens': ['error', 'always'],
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'comma-dangle': ['error', 'never'],
+        'comma-spacing': ['error', { before: false, after: true }],
+        'indent': ['error', 4],
+        'jsx-quotes': ['error', 'prefer-double'],
+        'linebreak-style': [0, 'error', 'windows'],
+        'max-len': ['error', { code: 120 }],
+        'no-duplicate-imports': ['error', {
+            includeExports: true
+        }],
+        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': ['error', 'always'],
+        'space-in-parens': ['error', 'never']
+    }
 };
