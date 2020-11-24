be71fb408f0722b0c017d3a85609b911659fcdc9
Evgeny <evge_nik@tut.by>
2020-11-24 17:10:28 +0300

    Models: remove validations from best-player model

--- app/models/best-player.js
+++ app/models/best-player.js
@@ -1,30 +1,6 @@
 import Model, { attr } from '@ember-data/model';
-import { validator, buildValidations } from 'ember-cp-validations';
 
-const Validations = buildValidations({
-    // firstName: {
-    //     description: 'First name',
-    //     validators: [
-    //         validator('presence', true),
-    //         validator('length', {
-    //             min: 2,
-    //             max: 20
-    //         })
-    //     ]
-    // },
-    // lastName: {
-    //     description: 'Last name',
-    //     validators: [
-    //         validator('presence', true),
-    //         validator('length', {
-    //             min: 2,
-    //             max: 20
-    //         })
-    //     ]
-    // }
-});
-
-export default Model.extend(Validations, {
+export default Model.extend({
     teamName: attr('string'),
     firstName: attr('string'),
     lastName: attr('string'),
