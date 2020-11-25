01362648c9bf7820b935f7dc98316907f3b7008f
Evgeny <evge_nik@tut.by>
2020-11-25 19:22:08 +0300

    Application: move pagination logic from controller to component

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,27 +3,11 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
-    get pageValues() {
-        const pages = Array.from({ length: Math.ceil(this.model.news.meta.count / this.limit) }, (_, i) => i + 1);
-
-        return pages;
-    }
-
     @tracked filterField;
     @tracked filterValue;
 
     @tracked page = 1;
     @tracked limit = 5;
-
-    limitValues = [5, 10, 20, 50];
-
-    resetPage() {
-        this.page = 1;
-    }
-
-    movePage(amount) {
-        this.page += amount;
-    }
 
     @action
     onItemClick(value) {
@@ -38,30 +22,8 @@ export default class IndexController extends Controller {
     }
 
     @action
-    onPageClick(page) {
-        this.page = page;
-    }
-
-    @action
-    onPageArrowClick(amount) {
-        if (this.pageValues.length === 0) {
-            return;
-        }
-
-        if (this.page === 1 && amount === -1) {
-            return;
-        }
-
-        if (this.page === this.pageValues.length && amount === 1) {
-            return;
-        }
-
-        this.movePage(amount);
-    }
-
-    @action
-    onLimitClick(limit) {
-        this.limit = limit;
-        this.resetPage();
+    updatePaginationGroup(newPage, newLimit) {
+        this.page = newPage;
+        this.limit = newLimit;
     }
 }
