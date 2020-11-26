8e28961bb1c117b61b7356e821685cccaa284b23
Evgeny <evge_nik@tut.by>
2020-11-26 13:47:57 +0300

    Application: add named property to model

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default class ApplicationRoute extends Route {
     model() {
-        return this.store.findAll('best-player');
+        return RSVP.hash({
+            bestPlayers: this.store.findAll('best-player')
+        });
     }
 }
